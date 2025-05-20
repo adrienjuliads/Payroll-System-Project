49643f24c94c881f1b65f8debede3371c4d45ba6
Julia <s2302482@usls.edu.ph>
2025-05-20 21:56:03 +0800

    updated comreport

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ package org.example;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
-        FirebaseConfig.initialize();
 
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new GUI().setVisible(true));
     }
 }
